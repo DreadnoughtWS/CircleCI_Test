449c7313b0b8efb3c8ff182583cf94c835ca5a76
“KevinG-88” <kevin.goutama@binus.ac.id>
2023-04-27 11:09:00 +0700

    prepare banner

---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/activity/banner/AllBannerListActivity.kt
@@ -0,0 +1,12 @@
+package com.academy.alfagiftmini.presentation.homepage.components.activity.banner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.academy.alfagiftmini.R
+
+class AllBannerListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_banner_list)
+    }
+}
