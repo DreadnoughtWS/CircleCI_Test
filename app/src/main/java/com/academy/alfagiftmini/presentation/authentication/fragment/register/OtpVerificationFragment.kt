2e963e49ba1b7e375ca327e136826569b7480a09
DreadnoughtWS <davidsiew14@gmail.com>
2023-04-27 15:55:00 +0700

    registration code and layout progress

--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.academy.alfagiftmini.R
 import com.academy.alfagiftmini.databinding.FragmentOtpVerificationBinding
+import com.academy.alfagiftmini.presentation.authentication.activity.RegisterActivity
 import kotlin.random.Random
 
 class OtpVerificationFragment : Fragment() {
@@ -24,13 +25,10 @@ class OtpVerificationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.btnGetOTPCode.setOnClickListener {
             //generate code
-            val generatedOTP = generateOTP()
+            (requireActivity() as RegisterActivity).getModel().generateOTP()
+            val generatedOTP = (requireActivity() as RegisterActivity).getModel().otp
             //create and send code via sms and broadcast receiver
-        }
-    }
 
-    private fun generateOTP(): String {
-        val otp = Random.nextInt(0,999999).toString()
-        return String.format("%06d", otp)
+        }
     }
 }
--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/viewmodel/RegisterViewModel.kt
@@ -3,6 +3,14 @@ package com.academy.alfagiftmini.presentation.authentication.viewmodel
 import androidx.lifecycle.ViewModel
 import com.academy.alfagiftmini.domain.register.RegisterDomainUseCase
 import javax.inject.Inject
+import kotlin.random.Random
 
 class RegisterViewModel @Inject constructor(private val useCase: RegisterDomainUseCase): ViewModel() {
+
+    private var _otp: String = "999999"
+    var otp = _otp
+    fun generateOTP() {
+        val otp = Random.nextInt(0,999999).toString()
+        _otp = String().format("%06d", otp)
+    }
 }
