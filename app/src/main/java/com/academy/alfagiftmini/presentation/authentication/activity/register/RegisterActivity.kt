a3fca687c614e7d84e7443c80959c487439222c4
DreadnoughtWS <davidsiew14@gmail.com>
2023-04-27 13:07:40 +0700

    registration layout progress

---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/activity/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.academy.alfagiftmini.presentation.authentication.activity.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.academy.alfagiftmini.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/activity/register/fragment/InputPhoneNumberFragment.kt
@@ -0,0 +1,18 @@
+package com.academy.alfagiftmini.presentation.authentication.activity.register.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.academy.alfagiftmini.R
+
+class InputPhoneNumberFragment : Fragment() {
+override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input_phone_number, container, false)
+    }
+}
