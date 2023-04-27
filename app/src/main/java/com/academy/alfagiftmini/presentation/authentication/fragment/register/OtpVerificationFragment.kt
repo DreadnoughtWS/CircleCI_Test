9cad5a146d1b552c309bdc5e4545c8f7928225a3
DreadnoughtWS <davidsiew14@gmail.com>
2023-04-27 16:39:22 +0700

    registration code and layout progress

--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.academy.alfagiftmini.R
 import com.academy.alfagiftmini.databinding.FragmentOtpVerificationBinding
 import com.academy.alfagiftmini.presentation.authentication.activity.RegisterActivity
@@ -23,12 +24,28 @@ class OtpVerificationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //belum di tes
+        (requireActivity() as RegisterActivity).getModel().timer.observe(viewLifecycleOwner, Observer {
+            binding.btnGetOTPCode.text = it.toString()
+        })
+        (requireActivity() as RegisterActivity).getModel().finished.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                binding.btnGetOTPCode.text = R.string.send_otp_code.toString()
+                setOTPGenerator()
+            }
+            else binding.btnGetOTPCode.setOnClickListener(null)
+        })
+        setOTPGenerator()
+    }
+
+    private fun setOTPGenerator() {
         binding.btnGetOTPCode.setOnClickListener {
             //generate code
             (requireActivity() as RegisterActivity).getModel().generateOTP()
             val generatedOTP = (requireActivity() as RegisterActivity).getModel().otp
             //create and send code via sms and broadcast receiver
             //set countdown timer in viewmodel to observe
+            (requireActivity() as RegisterActivity).getModel().otpCountdownTimer()
         }
     }
 }
--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/viewmodel/RegisterViewModel.kt
@@ -1,5 +1,8 @@
 package com.academy.alfagiftmini.presentation.authentication.viewmodel
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.academy.alfagiftmini.domain.register.RegisterDomainUseCase
 import javax.inject.Inject
@@ -12,5 +15,27 @@ class RegisterViewModel @Inject constructor(private val useCase: RegisterDomainU
     fun generateOTP() {
         val generatedOTP = Random.nextInt(0,999999).toString()
         _otp = String().format("%06d", generatedOTP)
+    }
+
+    private lateinit var _timer: MutableLiveData<Int>
+    private lateinit var _finished: MutableLiveData<Boolean>
+    var timer = _timer
+    var finished = _finished
+    fun otpCountdownTimer(){
+        _finished.value = false
+        object : CountDownTimer(30000, 1000) {
+
+            // Callback function, fired on regular interval
+            override fun onTick(millisUntilFinished: Long) {
+                val time = millisUntilFinished / 1000
+                _timer.value = time.toInt()
+            }
+
+            // Callback function, fired
+            // when the time is up
+            override fun onFinish() {
+                _finished.value = true
+            }
+        }.start()
     }
 }
