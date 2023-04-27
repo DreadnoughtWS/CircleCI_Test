87dac2e3ccad527c5256aeed41346cc8b8e3ee75
DreadnoughtWS <davidsiew14@gmail.com>
2023-04-27 15:44:49 +0700

    registration code and layout progress

--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/OtpVerificationFragment.kt
@@ -6,13 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.academy.alfagiftmini.R
+import com.academy.alfagiftmini.databinding.FragmentOtpVerificationBinding
+import kotlin.random.Random
 
 class OtpVerificationFragment : Fragment() {
-override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_otp_verification, container, false)
+    private lateinit var binding: FragmentOtpVerificationBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+        ): View {
+            // Inflate the layout for this fragment
+        binding = FragmentOtpVerificationBinding.inflate(inflater, container, false)
+        return binding.root
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGetOTPCode.setOnClickListener {
+            //generate code
+            val generatedOTP = generateOTP()
+            //create and send code via sms and broadcast receiver
+        }
+    }
+
+    private fun generateOTP(): String {
+        val otp = Random.nextInt(0,999999).toString()
+        return String.format("%06d", otp)
     }
 }
