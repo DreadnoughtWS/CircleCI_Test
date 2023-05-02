10a243919ea1acb90f3912eb45371a7869d3a23c
Kevin Goutama <kgoutama.kg@gmail.com>
2023-05-02 13:28:25 +0700

    progress

--- app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/adapter/banner/BannerBerandaSliderAdapter.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/adapter/banner/BannerBerandaSliderAdapter.kt
@@ -1,29 +1,47 @@
 package com.academy.alfagiftmini.presentation.homepage.components.adapter.banner
 
 import android.content.Context
-import android.view.View
+import android.content.Intent
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.academy.alfagiftmini.R
+import com.academy.alfagiftmini.databinding.ItemBannerSliderBinding
 import com.academy.alfagiftmini.domain.banner.model.BannerDomainModel
+import com.academy.alfagiftmini.presentation.PresentationUtils
+import com.academy.alfagiftmini.presentation.homepage.components.activity.banner.BannerPromoItemList
+import com.bumptech.glide.Glide
 import com.smarteist.autoimageslider.SliderViewAdapter
 
 class BannerBerandaSliderAdapter(
     private var listBanner: List<BannerDomainModel>, var context: Context
 ): SliderViewAdapter<BannerBerandaSliderAdapter.BannerAdapterViewHolder>() {
 
-    class BannerAdapterViewHolder(itemView: View) : ViewHolder(itemView) {
-
+    class BannerAdapterViewHolder(val binding: ItemBannerSliderBinding) : ViewHolder(binding.root) {
+        fun bindData(data: BannerDomainModel) {
+            Glide.with(itemView.context).load(data.bannerImageFileName).placeholder(R.drawable.banner_placeholder)
+                .into(binding.ivImageSlider)
+        }
     }
 
     override fun getCount(): Int {
         return listBanner.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?): BannerAdapterViewHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup): BannerAdapterViewHolder {
+        val binding =
+            ItemBannerSliderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        context = parent.context
+        return BannerAdapterViewHolder(binding)
     }
 
-    override fun onBindViewHolder(viewHolder: BannerAdapterViewHolder?, position: Int) {
-
+    override fun onBindViewHolder(viewHolder: BannerAdapterViewHolder, position: Int) {
+            viewHolder.bindData(listBanner[position])
+            viewHolder.binding.ivImageSlider.setOnClickListener {
+                val intent = Intent(context, BannerPromoItemList::class.java)
+                intent.putExtra(PresentationUtils.BANNER_ID, listBanner[position].id)
+                context.startActivity(intent)
+            }
     }
+
 
 }
