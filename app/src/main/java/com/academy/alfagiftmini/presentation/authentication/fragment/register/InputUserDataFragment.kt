47c130ebe983e83964f0fae870260c71ff5880ab
DreadnoughtWS <davidsiew14@gmail.com>
2023-05-03 10:36:42 +0700

    creating register layout and passing data between navigation

--- app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/InputUserDataFragment.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/authentication/fragment/register/InputUserDataFragment.kt
@@ -4,15 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.academy.alfagiftmini.R
+import com.academy.alfagiftmini.databinding.FragmentInputUserDataBinding
+import com.academy.alfagiftmini.databinding.FragmentOtpVerificationBinding
 
 class InputUserDataFragment : Fragment() {
+    private lateinit var binding: FragmentInputUserDataBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input_user_data, container, false)
+        binding = FragmentInputUserDataBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnSubmitUserData.setOnClickListener {
+                if (etEmail.text.isNullOrEmpty() || etFirstName.text.isNullOrEmpty() || etLastName.text.isNullOrEmpty() || etPassword.text.isNullOrEmpty()){
+                    Toast.makeText(activity, "input all field data", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                }
+            }
+        }
     }
 }
