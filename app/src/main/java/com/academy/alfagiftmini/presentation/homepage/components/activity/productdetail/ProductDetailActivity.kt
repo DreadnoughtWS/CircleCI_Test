9b7ea2ee7c5463070889e38cc2997a441ba9d086
Kevin Goutama <kgoutama.kg@gmail.com>
2023-05-04 14:21:38 +0700

    temporary push

---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/activity/productdetail/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.academy.alfagiftmini.presentation.homepage.components.activity.productdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.academy.alfagiftmini.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
