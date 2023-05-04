7deebdf2199855571c4f4136b26291efc5489e99
MadeReihan <reihanmade@gmail.com>
2023-05-04 10:21:03 +0700

    nambah official store di fragment beranda

---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/fragment/officialstore/FragmentOfficialStore.kt
@@ -0,0 +1,97 @@
+package com.academy.alfagiftmini.presentation.homepage.components.fragment.officialstore
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import com.academy.alfagiftmini.R
+import com.academy.alfagiftmini.databinding.FragmentOfficialStoreBinding
+import com.academy.alfagiftmini.presentation.PresentationUtils
+import com.academy.alfagiftmini.presentation.homepage.components.activity.officialstore.AllOfficialStoreActivity
+import com.academy.alfagiftmini.presentation.homepage.components.activity.officialstore.DetailOfficialStoreActivity
+import com.academy.alfagiftmini.presentation.homepage.components.adapter.officialstore.OfficialStore14Adapter
+import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.OfficialStoreViewModel
+import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.ProductCategoriesViewModel
+
+class FragmentOfficialStore(private val viewModel: OfficialStoreViewModel) : Fragment() {
+    private lateinit var binding: FragmentOfficialStoreBinding
+    private lateinit var adapter: OfficialStore14Adapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOfficialStoreBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setAdapter()
+        setObserver()
+        getDataFromApi()
+        setButtonLihatSemua()
+    }
+
+    private fun setButtonLihatSemua() {
+        binding.tvLihatSemuaOfficial.setOnClickListener {
+            val intent = Intent(requireContext(), AllOfficialStoreActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getDataFromApi() {
+        viewModel.get14OfficialStre()
+    }
+
+    private fun setObserver() {
+        viewModel.officialStore14.observe(requireActivity()) {
+            if (it.isNullOrEmpty()) {
+                setLihatSemua(PresentationUtils.HIDE_LIHAT_SEMUA)
+                if (PresentationUtils.isNetworkAvailable(requireContext())) {
+                    Toast.makeText(requireContext(), "Tidak ada internet", Toast.LENGTH_SHORT).show()
+                }
+                return@observe
+            }
+            if (it.size < 14) {
+                setLihatSemua(PresentationUtils.HIDE_LIHAT_SEMUA)
+                adapter.updateData(it)
+                return@observe
+            }
+            setLihatSemua(PresentationUtils.SHOW_LIHAT_SEMUA)
+            adapter.updateData(it.dropLast(1))
+
+        }
+    }
+
+    private fun setLihatSemua(banyakDataOfficialStore: Boolean) {
+        if (banyakDataOfficialStore == PresentationUtils.HIDE_LIHAT_SEMUA) {
+            binding.tvLihatSemuaOfficial.visibility = View.INVISIBLE
+        } else {
+            binding.tvLihatSemuaOfficial.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setAdapter() {
+        adapter = OfficialStore14Adapter().apply {
+            setOnItemClickListener { _, data ->
+                val intent = Intent(requireContext(), DetailOfficialStoreActivity::class.java)
+                intent.putExtra(PresentationUtils.INTENT_DATA, data)
+                startActivity(intent)
+            }
+        }
+
+        binding.apply {
+            rvOfficalStore.setHasFixedSize(true)
+            rvOfficalStore.layoutManager = GridLayoutManager(
+                requireContext(), 2, GridLayoutManager.HORIZONTAL, false
+            )
+            rvOfficalStore.adapter = adapter
+        }
+    }
+
+}
--- app/src/main/java/com/academy/alfagiftmini/presentation/homepage/fragment/FragmentBeranda.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/fragment/FragmentBeranda.kt
@@ -8,30 +8,42 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.academy.alfagiftmini.databinding.FragmentBerandaBinding
 import com.academy.alfagiftmini.presentation.factory.PresentationFactory
+import com.academy.alfagiftmini.presentation.homepage.components.fragment.officialstore.FragmentOfficialStore
 import com.academy.alfagiftmini.presentation.homepage.components.fragment.productcategories.FragmentProductCategories
+import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.OfficialStoreViewModel
 import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.ProductCategoriesViewModel
 
 
-class FragmentBeranda(private val viewModelFactory: PresentationFactory ): Fragment() {
+class FragmentBeranda(private val viewModelFactory: PresentationFactory) : Fragment() {
     private lateinit var binding: FragmentBerandaBinding
 
 
-    private val viewModel: ProductCategoriesViewModel by viewModels{
+    private val viewModel: ProductCategoriesViewModel by viewModels {
         viewModelFactory
     }
+
+    private val officialStoreViewModel: OfficialStoreViewModel by viewModels {
+        viewModelFactory
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setFragment(binding.flProductCategories.id, FragmentProductCategories(viewModel))
+        setFragment(binding.flOfficialStore.id, FragmentOfficialStore(officialStoreViewModel))
     }
 
     private fun setFragment(layoutId: Int, fragment: Fragment) {
         val fragmentManager = parentFragmentManager
-        fragmentManager.beginTransaction().replace(layoutId, fragment ).commit()
+        fragmentManager.beginTransaction().replace(layoutId, fragment).commit()
+    }
+
+    private fun setOfficialStoreFragment(layoutId: Int, fragment: Fragment) {
+        val fragmentManager = parentFragmentManager
+        fragmentManager.beginTransaction().replace(layoutId, fragment).commit()
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentBerandaBinding.inflate(inflater)
         return binding.root
