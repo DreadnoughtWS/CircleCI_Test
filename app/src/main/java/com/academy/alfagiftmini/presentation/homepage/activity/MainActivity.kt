6b49e8bb6052234ffa37e3a3cf42a7d946b632b3
EbelAlfie <davis.onde.wei@gmail.com>
2023-05-04 09:23:29 +0700

    produk categori

--- app/src/main/java/com/academy/alfagiftmini/presentation/homepage/activity/MainActivity.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/activity/MainActivity.kt
@@ -3,10 +3,16 @@ package com.academy.alfagiftmini.presentation.homepage.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.academy.alfagiftmini.MyApplication
 import com.academy.alfagiftmini.R
 import com.academy.alfagiftmini.databinding.ActivityMainBinding
+import com.academy.alfagiftmini.presentation.factory.PresentationFactory
+import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.ProductCategoriesViewModel
 import com.academy.alfagiftmini.presentation.homepage.fragment.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,7 +22,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pesananFragment: FragmentPesanan
     private lateinit var akunFragment: FragmentAkun
 
+    @Inject
+    lateinit var viewModelFactory: PresentationFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as MyApplication).appComponent.mainActivityInject(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,8 +34,8 @@ class MainActivity : AppCompatActivity() {
         setFragmentNavigation()
     }
 
-    fun setFragmentNavigation(){
-        berandaFragment = FragmentBeranda()
+    private fun setFragmentNavigation(){
+        berandaFragment = FragmentBeranda(viewModelFactory)
         belanjaFragment = FragmentBelanja()
         promoFragment = FragmentPromo()
         pesananFragment = FragmentPesanan()
--- app/src/main/java/com/academy/alfagiftmini/presentation/homepage/fragment/FragmentBeranda.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/fragment/FragmentBeranda.kt
@@ -5,22 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import com.academy.alfagiftmini.MyApplication
 import com.academy.alfagiftmini.R
+import com.academy.alfagiftmini.databinding.FragmentBelanjaBinding
+import com.academy.alfagiftmini.databinding.FragmentBerandaBinding
+import com.academy.alfagiftmini.databinding.FragmentProductCategoriesBinding
+import com.academy.alfagiftmini.presentation.factory.PresentationFactory
+import com.academy.alfagiftmini.presentation.homepage.activity.MainActivity
+import com.academy.alfagiftmini.presentation.homepage.components.fragment.productcategories.FragmentProductCategories
+import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.ProductCategoriesViewModel
+import javax.inject.Inject
 
 
-class FragmentBeranda : Fragment() {
+class FragmentBeranda(private val viewModelFactory: PresentationFactory ): Fragment() {
+    private lateinit var binding: FragmentBerandaBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    private val viewModel: ProductCategoriesViewModel by viewModels{
+        viewModelFactory
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFragment(binding.flProductCategories.id, FragmentProductCategories(viewModel))
+    }
+
+    private fun setFragment(layoutId: Int, fragment: Fragment) {
+        val fragmentManager = parentFragmentManager
+        fragmentManager.beginTransaction().replace(layoutId, fragment ).commit()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+    ): View {
+        binding = FragmentBerandaBinding.inflate(inflater)
+        return binding.root
     }
 
 
