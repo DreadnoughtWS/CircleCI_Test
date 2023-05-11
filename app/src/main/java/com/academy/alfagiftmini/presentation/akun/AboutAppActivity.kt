5cebb12ad643245d2f2eaaa65001cbbc17fe9abf
DreadnoughtWS <davidsiew14@gmail.com>
2023-05-11 16:35:47 +0700

    Set Akun Profile ui and edit activity setup

---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/akun/AboutAppActivity.kt
@@ -0,0 +1,12 @@
+package com.academy.alfagiftmini.presentation.akun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.academy.alfagiftmini.R
+
+class AboutAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
---
+++ app/src/main/java/com/academy/alfagiftmini/presentation/akun/EditAkunActivity.kt
@@ -0,0 +1,12 @@
+package com.academy.alfagiftmini.presentation.akun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.academy.alfagiftmini.R
+
+class EditAkunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_akun)
+    }
+}
