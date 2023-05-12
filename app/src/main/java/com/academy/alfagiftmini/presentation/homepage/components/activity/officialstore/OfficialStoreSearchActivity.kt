86d3cceee294f06f200d0c60008e54a8ab82f2fe
MadeReihan <reihanmade@gmail.com>
2023-05-12 12:57:02 +0700

    ganti progress.xml

--- app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/activity/officialstore/OfficialStoreSearchActivity.kt
+++ app/src/main/java/com/academy/alfagiftmini/presentation/homepage/components/activity/officialstore/OfficialStoreSearchActivity.kt
@@ -1,16 +1,20 @@
 package com.academy.alfagiftmini.presentation.homepage.components.activity.officialstore
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.academy.alfagiftmini.MyApplication
 import com.academy.alfagiftmini.R
 import com.academy.alfagiftmini.databinding.ActivityOfficialStoreSearchBinding
 import com.academy.alfagiftmini.presentation.PresentationUtils
+import com.academy.alfagiftmini.presentation.PresentationUtils.loadingAlertDialog
+import com.academy.alfagiftmini.presentation.PresentationUtils.setLoading
 import com.academy.alfagiftmini.presentation.factory.PresentationFactory
 import com.academy.alfagiftmini.presentation.homepage.components.adapter.officialstore.AllOfficialStorePagingAdapter
 import com.academy.alfagiftmini.presentation.homepage.components.viewmodel.OfficialStoreViewModel
@@ -21,6 +25,7 @@ import javax.inject.Inject
 class OfficialStoreSearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOfficialStoreSearchBinding
     private lateinit var adapter: AllOfficialStorePagingAdapter
+    private lateinit var dialog: Dialog
 
     @Inject
     lateinit var presentationFactory: PresentationFactory
@@ -33,10 +38,15 @@ class OfficialStoreSearchActivity : AppCompatActivity() {
         binding = ActivityOfficialStoreSearchBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setDialog()
         setHideToolbar()
         setToolbar()
         setAdapter()
         setDataSearch()
+    }
+
+    private fun setDialog() {
+        dialog = loadingAlertDialog(this)
     }
 
     private fun setDataSearch() {
@@ -65,6 +75,13 @@ class OfficialStoreSearchActivity : AppCompatActivity() {
                 )
                 intent.putExtra(PresentationUtils.INTENT_DATA, data)
                 startActivity(intent)
+            }
+        }
+        adapter.addLoadStateListener { loadState ->
+            if (loadState.refresh is LoadState.Loading) {
+                setLoading(true, dialog)
+            } else {
+                setLoading(false, dialog)
             }
         }
         binding.apply {
