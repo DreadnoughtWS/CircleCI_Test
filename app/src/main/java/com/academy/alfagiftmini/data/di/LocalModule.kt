c8ae615c416458c285087eca4de2bdf60ab3dec1
MadeReihan <reihanmade@gmail.com>
2023-05-21 12:03:14 +0700

    benerin toolbar detail_official_store_toolbar.xml

--- app/src/main/java/com/academy/alfagiftmini/data/di/LocalModule.kt
+++ app/src/main/java/com/academy/alfagiftmini/data/di/LocalModule.kt
@@ -11,19 +11,21 @@ import dagger.Provides
 
 @Module
 @Database(entities = [RiwayatPencarianDataModel::class], version = 1)
-abstract class LocalModule: RoomDatabase(){
-    abstract fun riwayatPencarianDao() : RiwayatPencarianDao
+abstract class LocalModule : RoomDatabase() {
+    abstract fun riwayatPencarianDao(): RiwayatPencarianDao
+
     companion object {
         @Volatile
         private var Object: LocalModule? = null
+
         @Provides
         @JvmStatic
         fun getDatabase(context: Context): LocalModule {
             if (Object == null) {
                 synchronized(LocalModule::class.java) {
-                    Object = Room.databaseBuilder(context.applicationContext,
-                        LocalModule::class.java, "app_db")
-                        .build()
+                    Object = Room.databaseBuilder(
+                        context.applicationContext, LocalModule::class.java, "app_db1"
+                    ).build()
                 }
             }
             return Object as LocalModule
